53ec0d1cdaee5895c7093631e9a330ff287f7af4
rishabhanand2310 <41969091+rishabhanand2310@users.noreply.github.com>
2018-08-08 19:01:44 +0530

    Commit @ 8 Aug | 7 PM

---
+++ src/ObjectArray.java
@@ -0,0 +1,25 @@
+
+public class ObjectArray {
+	
+	
+	public static void main (String args[]) {
+		
+		Object ob[] = new Object[8];
+		
+		ob[0]="abc";
+		ob[1]=3;
+		ob[2]='e';
+		ob[3]="30/2/1945";
+		ob[4]=39;
+		
+		System.out.println(ob[1]);
+		
+		for (Object i : ob) {
+			
+			System.out.println("value is : "+i);
+		}
+		
+		
+	}
+
+}
