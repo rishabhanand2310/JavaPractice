d169173bc120e407d65fa5c7687a27b5ab6a5754
rishabhanand2310 <41969091+rishabhanand2310@users.noreply.github.com>
2018-08-09 17:14:33 +0530

    Commit @ 9 Aug | 5:14 pm

---
+++ src/OOPs/CalByValueAndCallByRef.java
@@ -0,0 +1,46 @@
+package OOPs;
+
+public class CalByValueAndCallByRef {
+	
+	int p=1;
+	int q=100;
+
+	public static void main(String[] args) {
+		// Call by value
+		
+		CalByValueAndCallByRef obj=new CalByValueAndCallByRef();
+		int sum=obj.testsum(10,12);
+		System.out.println(sum);
+		
+		// Or
+		int x=10;
+		int y=12;
+		int sum1=obj.testsum(x,y);
+		System.out.println(sum1);
+		
+		
+		// call by reference
+		
+		obj.refswap(obj);
+		
+		System.out.println("value of  p " + obj.p);
+		System.out.println("value of  q " + obj.q);
+	}
+
+	public int testsum(int a, int b) {
+		
+		int c=a+b;
+		return c;
+	}
+	
+	// method for call by reference
+	public void refswap(CalByValueAndCallByRef t) {
+		
+		int temp;
+		temp=t.p;
+		t.p=t.q;
+		t.q=temp;
+		
+	}
+	
+}
---
+++ src/OOPs/WrapperClass.java
@@ -0,0 +1,15 @@
+package OOPs;
+
+public class WrapperClass {
+	
+	public static void main (String args[]) {
+		
+		String x="100";
+		
+		System.out.println(x+20); // 10020
+		
+		System.out.println(Integer.parseInt(x)+20); // 120 ---- Integer is Wrapper class // Boolean,  Double
+	
+	}
+
+}
